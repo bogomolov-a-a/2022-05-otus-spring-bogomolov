78533467131f10cf1dcab00b1c6529da1a549590
bogomolov-a-a <artem.bogomolov.a@gmail.com>
2022-11-24 18:49:19 +0300

    feign component call fixed.

--- 19-homework-diploma-project/zooshop-category-service/src/main/java/ru/otus/group202205/homework/spring19/zooshop/category/feign/GoodServiceFeignProxy.java
+++ 19-homework-diploma-project/zooshop-category-service/src/main/java/ru/otus/group202205/homework/spring19/zooshop/category/feign/GoodServiceFeignProxy.java
@@ -1,13 +1,13 @@
 package ru.otus.group202205.homework.spring19.zooshop.category.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "zooshop-good-service")
 public interface GoodServiceFeignProxy {
 
-  @GetMapping("/by/category/{categoryId}")
+  @DeleteMapping("/by/category/{categoryId}")
   void deleteAllByCategory(@PathVariable("categoryId") Long categoryId);
 
 }
